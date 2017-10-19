b050bc82f72064303c283cc071098436e732801d
apexa11 <apexachoxi7@gmail.com>
2017-10-19 11:58:35 -0400

    creating hash password

--- main.py
+++ main.py
@@ -12,3 +12,16 @@ import jinja2
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
+
+secret = 'Happy11d22'
+
+def hashPassword(password, username):
+    return hashlib.sha256(password + username + secret).hexdigest()
+
+def make_secure_val(val):
+    return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
+
+def check_secure_val(secure_val):
+    val = secure_val.split('|')[0]
+    if secure_val == make_secure_val(val):
+        return val
