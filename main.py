be6ac0f457bf9488b369571af2c8a9c63db4ebd4
apexa11 <apexachoxi7@gmail.com>
2017-10-19 12:00:13 -0400

    creating handler

--- main.py
+++ main.py
@@ -38,7 +38,32 @@ def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
 
+class MasterHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
 
+    def render_str(self, template, **params):
+        params['user'] = self.user
+        return render_str(template, **params)
+        
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+    def set_secure_cookie(self, name, val):
+        cookie_val = make_secure_val(val)
+        self.response.headers.add_header(
+            'Set-Cookie',
+            '%s=%s; Path=/' % (name, cookie_val))
+
+    def read_secure_cookie(self, name):
+        cookie_val = self.request.cookies.get(name)
+        return cookie_val and check_secure_val(cookie_val)
+
+    def logout(self):
+        self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
+
+    def error(self):
+        self.render('error.html')
 
 
     
