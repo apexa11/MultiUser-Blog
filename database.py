f7595f54bb84a79e3e0232ee2d32329addf6746a
apexa11 <apexachoxi7@gmail.com>
2017-10-19 11:46:42 -0400

    creating database

--- database.py
+++ database.py
@@ -6,3 +6,20 @@ class Post(ndb.Model):
     post_author = ndb.StringProperty(required = True)
     post_created = ndb.DateTimeProperty(auto_now_add = True)
     post_last_updated = ndb.DateTimeProperty(auto_now = True)
+
+     @classmethod
+    def addPost(cls, title, content, author):
+        p = Post(post_title = title, post_content = content,
+                 post_author = author)
+        p.put()
+        return p.key.id()
+
+    @classmethod
+    def editPost(cls, title, content, author, post_id):
+        post = Post.get_by_id(int(post_id))
+        if post:
+            if post.post_author == author:
+                post.post_title = title
+                post.post_content = content
+                post.put()
+                return post.key.id()
