724fd9a1b4fe1fb4c276ef29839a45b100367cd4
apexa11 <apexachoxi7@gmail.com>
2017-10-19 11:59:35 -0400

    creating hash password

--- main.py
+++ main.py
@@ -25,3 +25,20 @@ def check_secure_val(secure_val):
     val = secure_val.split('|')[0]
     if secure_val == make_secure_val(val):
         return val
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
+
+
+
+
+    
